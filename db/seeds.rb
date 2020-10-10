8b4ffef423fe8199ef7880f6a33402c4586b6995
keitarou516 <keitarou516@icloud.com>
2020-10-11 05:31:52 +0900

    added data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ require "csv"
 
 CSV.foreach('db/csv/rice.csv', headers: true) do |row|
   Rice.create(
+    name: row['name'],
     condition: row['condition'],
     hard: row['hard'],
     time: row['time'],
