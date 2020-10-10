cefb672b80170ee752c7b5be5eb4a5d88b67ab47
keitarou516 <keitarou516@icloud.com>
2020-10-11 06:15:52 +0900

    added data

--- app/controllers/rices_controller.rb
+++ app/controllers/rices_controller.rb
@@ -1,6 +1,7 @@
 class RicesController < ApplicationController
-    def index
+    def create
         answers = {answer1: 1, answer2: 1, answer3: 1} #paramsに変更する
+        answers = params[:selections]
         
         rices = Rice.where(condition: answers[:answer1]).where(hard: answers[:answer2]).where(time: answers[:answer3])
         render :json => rices
