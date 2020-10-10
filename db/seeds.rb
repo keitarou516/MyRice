7636a93c3d8f4bbd05b281e8b403e609926e2f36
keitarou516 <keitarou516@icloud.com>
2020-10-11 01:37:32 +0900

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+CSV.foreach('db/csv/content.csv', headers: true) do |row|
+  Rice.create(
+    condition: row['condition'],
+    hard: row['hard'],
+    time: row['time'],
+    stickness: row['stickness'],
+    url: row['url']
+  )
+end
