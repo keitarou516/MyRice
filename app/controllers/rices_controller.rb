1939c2180337d510da83be66a868ad386bbcc3f8
EC2 Default User <ec2-user@ip-172-31-11-68.ap-northeast-1.compute.internal>
2020-10-10 14:52:48 +0000

    route rice_controller commit

--- app/controllers/rices_controller.rb
+++ app/controllers/rices_controller.rb
@@ -1,2 +1,11 @@
 class RicesController < ApplicationController
+    def create
+        answers = {answer1: 1, answer2: 1, answer3: 1} #paramsに変更する
+        
+        rices = Rice.where(condition: answers[:answer1]).where(hard: answers[:answer2]).where(time: answers[:answer3])
+        render :json => rices
+    end
+        
 end
+
+
